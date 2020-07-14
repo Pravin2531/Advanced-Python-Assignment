42692da90d43b1dae702f4914fecee66692283e2
Pravin2531 <68236629+Pravin2531@users.noreply.github.com>
2020-07-14 20:38:32 +0530

    Add files via upload

---
+++ Contact Details.py
@@ -0,0 +1,10 @@
+
+Name = input("Please enter your name ")
+Age = input("Please enter your age ")
+Course = input("Please enter your course ")
+Contact = input("Please enter your mobile number ")
+print("\n")
+print("The name you had entered", Name)
+print("The age you had entered", Age)
+print("The course you had entered", Course)
+print("The mobile number you had entered", Contact)
