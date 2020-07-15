35eadbda5e2462316de5d75956bdc3333e5ec10f
Pravin2531 <68236629+Pravin2531@users.noreply.github.com>
2020-07-15 20:46:11 +0530

    Add files via upload

---
+++ DecisionMaking.py
@@ -0,0 +1,11 @@
+
+number1 = int(input("Enter the first number "))
+number2 = int(input("Enter the second number "))
+if number1>number2:
+    result = number1-number2
+    print(f"Subtraction of {number1}-{number2} is ", result)
+
+else:
+    result = number2-number1
+    print(f"Subtraction of {number2}-{number1} is  ", result)
+
